eb6ff9f36ab743c0f28e0a9ff55dcb2c2cd7523d
idaltchion <idaltchion@gmail.com>
2023-10-01 20:21:20 -0300

    feat(security): configure password, authentication code + pkce, client credentials and implicit flows

--- src/main/java/com/idaltchion/ifxfood/auth/config/AuthorizationServerConfig.java
+++ src/main/java/com/idaltchion/ifxfood/auth/config/AuthorizationServerConfig.java
@@ -1,5 +1,7 @@
 package com.idaltchion.ifxfood.auth.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,6 +12,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.CompositeTokenGranter;
+import org.springframework.security.oauth2.provider.TokenGranter;
 
 @Configuration
 @EnableAuthorizationServer
@@ -27,12 +31,36 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory()
+			/* client utilizado no Password Credentials Flow */
 			.withClient("ifxfood-web")
 				.secret(passwordEncoder.encode("12345"))
 				.authorizedGrantTypes("password", "refresh_token")
 				.scopes("read", "write")
 				.accessTokenValiditySeconds(6 * 60 * 60) //6 horas
 				.refreshTokenValiditySeconds(24 * 60 * 60) //24 horas
+		.and()
+			/* client utilizado no Client Credentials Flow */
+			.withClient("ifxfood-thirty")
+				.secret(passwordEncoder.encode("12345"))
+				.authorizedGrantTypes("client_credentials")
+				.scopes("read", "write")
+		.and()
+			/* client utilizado no Authorization Code Flow 
+			 * redirect-url: http://auth.ifxfood.local:8081/oauth/authorize?response_type=code&client_id=<client-id>&state=<state>&redirect_uri=<url de redirecionamento>
+			 * */
+			.withClient("ifxfood-code")
+				.secret(passwordEncoder.encode("12345"))
+				.authorizedGrantTypes("authorization_code")
+				.redirectUris("http://ui.ifxfood.local:8082")
+				.scopes("read", "write")
+		.and()
+			/* client utilizado no Implicit Flow 
+			 * redirect-url: http://auth.ifxfood.local:8081/oauth/authorize?response_type=token&client_id=<client-id>&state=<state>&redirect_uri=<url de redirecionamento>
+			 * */
+			.withClient("ifxfood-implicit")
+				.authorizedGrantTypes("implicit")
+				.redirectUris("http://ui.ifxfood.local:8082")
+				.scopes("read", "write")
 		.and()
 			/* esse client tem a funcao de somente validar o token pelo resource server (/check_token) */
 			.withClient("resource-server-client") 
@@ -48,7 +76,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		endpoints
 			.authenticationManager(authenticationManager)
 			.userDetailsService(userDetailsService)
-			.reuseRefreshTokens(false);
+			.reuseRefreshTokens(false)
+			.tokenGranter(tokenGranter(endpoints));
 	}
 	
 	@Override
@@ -56,5 +85,17 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 //		security.checkTokenAccess("permitAll()");
 		security.checkTokenAccess("isAuthenticated()");
 	}
+	
+	private TokenGranter tokenGranter(AuthorizationServerEndpointsConfigurer endpoints) {
+		var pkceAuthorizationCodeTokenGranter = new PkceAuthorizationCodeTokenGranter(endpoints.getTokenServices(),
+				endpoints.getAuthorizationCodeServices(), endpoints.getClientDetailsService(),
+				endpoints.getOAuth2RequestFactory());
+		
+		var granters = Arrays.asList(
+				pkceAuthorizationCodeTokenGranter, endpoints.getTokenGranter());
+		
+		return new CompositeTokenGranter(granters);
+	}
+	
 	
 }
